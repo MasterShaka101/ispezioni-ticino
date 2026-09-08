4bfe9a5c1d70bd7a7d42caadfe8a42f18fd7695d
MasterShaka101 <alessio.salmina@ti.ch>
2026-09-08 13:33:13 +0200

    Update Supabase URL to remove endpoint path

--- admin.js
+++ admin.js
@@ -2,7 +2,7 @@
 // CONFIGURAZIONE SUPABASE
 // ========================================
 
-const SUPABASE_URL = "https://htcuwuhebznznjpizepz.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://htcuwuhebznznjpizepz.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_AVWMx6QAsgorykTKjF8RGA_xYSw9G_O";
 
